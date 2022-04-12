29b213672eb25f7e39d1cfa8d396c83f8f9ce237
djordje997 <86196338+djordje997@users.noreply.github.com>
2022-04-12 13:49:15 +0200

    G2 atom feed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -60,6 +60,16 @@ class ProductsController < ApplicationController
       format.json { head :no_content }
     end
   end
+  
+  def who_bought
+    @product = Product.find(params[:id])
+    @latest_order = @product.orders.order(:updated_at).last
+    if stale?(@latest_order)
+      respond_to do |format|
+        format.atom
+      end
+    end
+  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
@@ -77,4 +87,5 @@ class ProductsController < ApplicationController
     def product_params
       params.require(:product).permit(:title, :description, :image_url, :price)
     end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
+  has_many :line_items
   has_many :orders, through: :line_items
   validates :title, :description, :image_url, presence: true
   validates :title, uniqueness: true
@@ -8,7 +9,7 @@ class Product < ApplicationRecord
     }
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates_length_of :title, :minimum => 10 , message: 'must be at least 10 charachters'
-  has_many :line_items
+  
   before_destroy :ensure_not_referenced_by_any_line_item
   private
 
