9c84762a728a52beb4736b857e7e115ff3e9dcb4
djordje997 <86196338+djordje997@users.noreply.github.com>
2022-04-28 12:58:32 +0200

    page 303 iteration L2

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,21 @@ module Depot
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
     config.middleware.use I18n::JS::Middleware
+
+    config.to_prepare do 
+      #taken from https://github.com/rails/rails/issues/43928 once rails issue is resolved we can remove this code
+      # some monkey patches
+      Rails::Conductor::ActionMailbox::InboundEmailsController.class_eval do
+        private
+        def new_mail
+          Mail.new(mail_params.except(:attachments).to_h).tap do |mail|
+            mail[:bcc]&.include_in_headers = true
+            mail_params[:attachments].to_a.compact_blank.each do |attachment|
+              mail.add_file(filename: attachment.original_filename, content: attachment.read)
+            end
+          end
+        end
+      end    
+    end
   end
 end
